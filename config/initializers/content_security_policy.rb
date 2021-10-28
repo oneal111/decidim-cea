7d446a67e8763eaaaa69a7ffdc9e0d4ef8e1cf84
MoretS <s.moret@ecole-ipssi.net>
2021-10-28 22:14:32 +0200

    Linting files

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -7,13 +7,12 @@
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
 Rails.application.config.content_security_policy do |policy|
-  policy.default_src :self, :https, "argus.osp.cat", Decidim::Organization.all.collect { |org| "https://#{org.host}" }.join(',')
+  policy.default_src :self, :https, "argus.osp.cat", Decidim::Organization.all.collect { |org| "https://#{org.host}" }.join(",")
   policy.font_src :self, :https, :data
   policy.img_src :self, :https, :data
   policy.object_src :none
   policy.script_src :self, :https, :unsafe_inline, :unsafe_eval
   policy.style_src :self, :https, :unsafe_inline
-
 
   # Specify URI for violation reports
   # policy.report_uri "/csp-violation-report-endpoint"
