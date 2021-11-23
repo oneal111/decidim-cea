855706e68fac5be7daecdafb51f7471e1ad71e40
Armand Fardeau <armandfardeau@users.noreply.github.com>
2021-11-23 17:57:08 +0100

    Add integrity and defer to assets (#14)
    * Add integrity and defer to assets
    
    * Remove defer attributes
    
    * Fix empty line
    
    Co-authored-by: moustachu <moustachu@opensourcepolitics.eu>

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
 # Default CarrierWave setup.
-#
-CarrierWave.configure do |config|
-  config.permissions = 0o666
-  config.directory_permissions = 0o777
-  config.storage = :file
-  config.enable_processing = !Rails.env.test?
-  # This needs to be set for correct attachment file URLs in emails
-  # DON'T FORGET to ALSO set this in `config/application.rb`
-  # config.asset_host = "https://your.server.url"
+unless Rails.env.test?
+  CarrierWave.configure do |config|
+    config.permissions = 0o666
+    config.directory_permissions = 0o777
+    config.storage = :file
+    config.enable_processing = true
+    # This needs to be set for correct attachment file URLs in emails
+    # DON'T FORGET to ALSO set this in `config/application.rb`
+    config.asset_host = "https://entre-vos-mains.alsace.eu"
+  end
 end
 
 # Setup CarrierWave to use Amazon S3. Add `gem "fog-aws" to your Gemfile.
